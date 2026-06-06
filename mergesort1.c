0d0fa51d648a0e3d37a25760dcf7a05a6e36a727
kislay59 <kislaychaturvedi.cs24@bmsce.ac.in>
2026-06-06 20:34:54 +0530

    Updated mergesort1.c for ADA lab exam

--- mergesort1.c
+++ mergesort1.c
@@ -2,26 +2,26 @@
 #include <stdlib.h>
 #include <time.h>
 
-void merge(int A[], int left, int mid, int right)
+void merge(int A[], int left, int mid, int right) // COMBINES SORTED HALVES
 {
-    int n1=(mid-left+1);
+    int n1=(mid-left+1); //SIZES
     int n2=(right-mid);
 
-    int L [n1];
+    int L [n1];//LEFT AND RIGHT HALF ARRAY COPIES
     int R[n2];
     for(int i=0;i<n1;i++)
     {
-        L[i]=A[left+i];
+        L[i]=A[left+i];//COPYING LEFT HALF
     }
     for(int j=0;j<n2;j++)
     {
         R[j]=A[mid+1+j];
     }
 
-    int i=0,j=0,k=left;
+    int i=0,j=0,k=left;//i= L pointer , j= R pointer , k= Original array pointer <=== IMPORTANT
 
-    while(i<n1 && j<n2)
-    {
+    while(i<n1 && j<n2)//BOTH HAVE ELEMENTS
+    {// COMPARE
         if(L[i]<=R[j])
         {
             A[k]=L[i];
@@ -48,19 +48,19 @@ void merge(int A[], int left, int mid, int right)
 }
 void mergesort(int A[], int left, int right)
 {
-    if(left<right)
+    if(left<right) //MORE THAN ONE ELEMENT EXISTS
     {
         int mid=(left+right)/2;
 
-    mergesort(A,left,mid);
-    mergesort(A,mid+1,right);
-    merge(A,left,mid,right);
+    mergesort(A,left,mid); //SORT LEFT HALF
+    mergesort(A,mid+1,right); //SORT RIGHT HALF
+    merge(A,left,mid,right); // MERGE THEM
     }
 }
 int main()
 {
-    clock_t start,end;
-    double timet;
+    clock_t start,end;//CLOCK VARIABLES
+    double timet;//TIME
     int n;
     printf("Enter no. of elements : ");
     scanf("%d",&n);
@@ -73,7 +73,7 @@ int main()
         arr[i] = rand() % n;
     }
 
-    start=clock();
+    start=clock();//TIME FUNCTION
     mergesort(arr,0,n-1);
     end=clock();
 
@@ -82,6 +82,6 @@ int main()
     {
         printf("%d",arr[i]);
     }
-    timet=(double)(end-start)*1000/CLOCKS_PER_SEC;
+    timet=(double)(end-start)*1000/CLOCKS_PER_SEC;//CALCULATION
     printf("Time Taken : %f",timet);
 }
